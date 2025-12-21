0755f64b0d26c7424cf47ec76d5d0faf46477f74
marcoduartestorm <marcosbottocomp@gmail.com>
2025-12-21 05:24:22 -0300

    Alteração

--- src/main/java/com/apirestspringboot/apirest/ApirestApplication.java
+++ src/main/java/com/apirestspringboot/apirest/ApirestApplication.java
@@ -3,7 +3,7 @@ package com.apirestspringboot.apirest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+//Alteração
 @SpringBootApplication 
 public class ApirestApplication {
 
